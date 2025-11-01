e30818eec5e037dc73d5a6e8d6de8577ff423893
Akezhan Mussa <ake.mussa@gmail.com>
2025-11-01 13:40:43 +0000

    [backend] set port 8080

--- backend/main.py
+++ backend/main.py
@@ -365,4 +365,4 @@ async def test_route2():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
